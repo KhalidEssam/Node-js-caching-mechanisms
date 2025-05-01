f6ed67935637fcbe9a81c57280b0f0fe97038fb1
khalid <khaledesam384@gmail.com>
2025-05-01 22:53:14 +0300

    feat(config): add Redis client configuration

---
+++ src/config/redisClient.js
@@ -0,0 +1,9 @@
+const { createClient } = require('redis');
+require('dotenv').config();
+
+const client = createClient({ url: process.env.REDIS_URL });
+client.on('error', err => console.error('Redis Error', err));
+
+(async () => { await client.connect(); })();
+
+module.exports = client;
